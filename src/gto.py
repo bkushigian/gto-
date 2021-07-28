bab5bc1156bb929459112a28dc32f99d171205cb
Ben Kushigian <bkushigian@gmail.com>
2021-07-27 21:57:14 -0400

    Bug fix, cleanup after getting to run

--- src/gto.py
+++ src/gto.py
@@ -3,6 +3,7 @@
 """This file communicates with a GTO instance"""
 
 import socket
+import time
 
 class GTO:
 
@@ -14,6 +15,10 @@ class GTO:
     def connect(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((self.addr, self.port))
+        self.send("init")
+        self.sock.recv(5096)
+        resp = self.sock.recv(5096)
+        print(resp)
 
     def disconnect(self):
         if not self.sock:
@@ -22,8 +27,8 @@ class GTO:
 
     def load_file(self, path: str):
         self.send("Load file: {}".format(path))
-        recv = self.receive()
-        return recv
+        # recv = self.receive()
+        # return recv
 
     def request_node_data(self):
         return self.send('Request node data')
@@ -37,21 +42,31 @@ class GTO:
     def take_action(self, action_n):
         return self.send('Take action: {}'.format(action_n))
 
-    def request_action_data():
+    def request_action_data(self):
         return self.send('Request action data')
+
+    def ask_if_processing(self):
+        return self.send('Still processing instruction?')
 
     def create_message(self, message: str):
         return "~{}~".format(message)
 
-    def receive(self):
+    def receive(self, timeout_ms=1000):
+        now = time.time()
         chunks = []
         received = 0
         while True:
-            chunk = self.sock.recv(0)
+            chunk = self.sock.recv(5096)
+            if chunk == b'~Solver still running. Please try again later.~':
+                print('still processing...', time.time() - now)
+                if time.time() - now > timeout_ms:
+                    raise TimeoutError()
+                time.sleep(2.0)
+                continue
             received += len(chunk)
-            if len(chunk) == 0:
-                return b''.join(chunks)
             chunks.append(chunk)
+            if len(chunk) < 5096:
+                return b''.join(chunks)
 
 
     def send(self, message: str):
@@ -63,3 +78,20 @@ class GTO:
             if sent == 0:
                 raise RuntimeError("socket connection broken")
             total_sent += sent
+
+
+def test():
+    g = GTO()
+    g.connect()
+    g.load_file(r"C:\Users\bkush\Documents\GTO+\Kd3s2c-7-6-2021.gto")
+    print(g.receive())
+    g.request_node_data()
+    print(g.receive())
+    g.request_action_data()
+    print(g.receive())
+
+    g.request_current_line()
+    print(g.receive())
+
+if __name__ == '__main__':
+    test()
