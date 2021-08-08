a696ece2d2810242d51a6b96106e38262e1e165c
Ben Kushigian <bkushigian@gmail.com>
2021-08-08 13:08:26 -0400

    Improvements, tests

--- src/gto.py
+++ src/gto.py
@@ -4,6 +4,7 @@
 
 import socket
 import time
+from pathlib import Path
 
 class GTO:
 
@@ -16,35 +17,107 @@ class GTO:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((self.addr, self.port))
         self.send("init")
-        self.sock.recv(5096)
-        resp = self.sock.recv(5096)
-        print(resp)
+        self.sock.recv(4096)
+        resp = self.sock.recv(4096).decode().strip('~')
+        return resp
 
     def disconnect(self):
         if not self.sock:
             print("Warning: trying to close unopened connection")
         self.sock.close()
+        self.sock = None
 
     def load_file(self, path: str):
         self.send("Load file: {}".format(path))
         time.sleep(0.5)
-        recv = self.receive().decode()
+        recv = self.receive().decode().strip('~')
         return recv
 
+    def __parse_player_data(self, player_data_str, actions):
+        pos = None
+        pd = player_data_str.strip().split('\r\n')
+        meta = pd[0].strip().split(', ')
+        if len(meta) == 3:
+            pos = True
+
+        pd = [line.split() for line in pd[2:]]
+        pd_d = {}
+        for row in pd:
+            hand = row[0]
+            row_d = {}
+            rowf = [float(x) for x in row[1:]]
+            row_d['COMBOS'] = rowf[0]
+            row_d['EQUITY'] = rowf[1]
+            if pos == True:
+                for i, a in enumerate(actions):
+                    row_d[a] = {'FREQ': rowf[2+i], 'EV': rowf[2+i+len(actions)]}
+            pd_d[hand] = row_d
+        return pd_d, pos
+
     def request_node_data(self):
+        '''
+        Return a dict representing node date. The dict has keys:
+        + `board`: map to a list of board cards, in order
+        + `actions`: list of actions performed by the current player
+        + `pos`: current player ('ip', or 'oop')
+        + `oop`: a dict mapping OOP's hands to that hand's stats (see below)
+        + `ip`: a dict mapping IP's hands to that hand's stats (see below)
+        >>> s = GTO()
+        >>> s.connect()
+        'You are connected to GTO+'
+        >>> s._load_akq_game()
+        >>> nd = s.request_node_data()
+        >>> nd['pos']
+        'oop'
+        >>> nd['actions']
+        ['Bet 1', 'Check']
+        >>> nd['oop']['KcKh']['COMBOS']
+        1.0
+        >>> nd['oop']['KcKh']['EQUITY']
+        50.0
+        >>> nd['oop']['KcKh']['Bet 1']['FREQ']
+        0.0
+        >>> nd['oop']['KcKh']['Check']['FREQ']
+        100.0
+        >>> nd['oop']['KcKh']['Check']['EV']
+        0.25
+        >>> nd['ip']['QcQh']['COMBOS']
+        1.0
+        >>> nd['ip']['QcQh']['EQUITY']
+        0.0
+        '''
         self.send('Request node data')
-        time.sleep(0.5)
+        time.sleep(0.1)
         recv = self.receive().decode().strip('~').strip('[]')
         items = recv.split('][')
         _, board, oop, ip = items
 
-        return {'board': board, 'oop': oop, 'ip': ip}
+        # Get actions at current node.
+        actions = self.request_action_data()
+
+        pos = None                            # The position/player to act next
+
+        # Parse board of form "Board: AsTd3h4d"
+        board = board.split()[1].strip()
+        board = [ board[i:i+2] for i in range(0, len(board), 2)]
+
+        # Parse oop
+        oop, oop_to_act = self.__parse_player_data(oop, actions)
+        if oop_to_act:
+            pos = 'oop'
+
+        # Parse ip
+        ip, ip_to_act = self.__parse_player_data(ip, actions)
+        if ip_to_act:
+            assert pos is None
+            pos = 'ip'
+        assert pos is not None
+        return {'board': board, 'oop': oop, 'ip': ip, 'pos': pos, 'actions': actions}
 
     def request_pot_stacks(self):
         self.send('Request pot/stacks')
         time.sleep(0.1)
         recv = self.receive().decode().strip('~')
-        print(recv)
 
         # Skip Header
         idx = recv.index(']')
@@ -70,19 +143,20 @@ class GTO:
         self.send('Request current line')
         time.sleep(0.05)
         recv = self.receive().decode().strip('~')
-        if recv == 'Hand is at  start of tree.':
+        if recv == 'Hand is at start of tree.':
             return []
         return recv.split(',')
 
     def take_action(self, action_n):
-        return self.send('Take action: {}'.format(action_n))
+        self.send('Take action: {}'.format(action_n))
+        time.sleep(0.05)
+        self.receive().decode().strip('~')
 
     def request_action_data(self):
         self.send('Request action data')
         time.sleep(0.1)
         recv = self.receive().decode().strip('~')
         recv = recv.strip(']').split(': ')[1].split(',')
-        print(recv)
         return  recv
 
     def ask_if_processing(self):
@@ -91,30 +165,46 @@ class GTO:
     def create_message(self, message: str):
         return "~{}~".format(message)
 
-    def receive(self, timeout_ms=1000):
-        now = time.time()
+    def receive(self, timeout_sec=1.0):
         chunks = []
         received = 0
+
+        # This loop collects 'chunks' of length 4096 until there is no more to
+        # collect (the sent message has been read in its entirety)
+        
         while True:
-            chunk = self.sock.recv(5096)
+            chunk = self.sock.recv(4096)
             if chunk == b'~Solver still running. Please try again later.~':
-                print('still processing...', time.time() - now)
-                if time.time() - now > timeout_ms:
-                    raise TimeoutError()
-                time.sleep(2.0)
+                print('still processing...')
+                time.sleep(0.5)
                 continue
             received += len(chunk)
             chunks.append(chunk)
-            if len(chunk) < 5096:
-                return b''.join(chunks)
+            if len(chunk) < 4096:
+                break
+
+        return b''.join(chunks)
 
 
-    def send(self, message: str):
+    def send(self, message: str, verbose=False):
         message = self.create_message(message).encode('utf-8')
         total_sent = 0
-        print("Sending payload: \"{}\"".format(message))
+        if verbose:
+            print("Sending payload: \"{}\"".format(message))
         while total_sent < len(message):
             sent = self.sock.send(message[total_sent:])
             if sent == 0:
                 raise RuntimeError("socket connection broken")
             total_sent += sent
+    
+    def _load_akq_game(self):
+        '''
+        load a dummy file holding the AKQ game strategy. This is useful for testing
+        and for learning the API
+        '''
+        akq_game = Path(__file__).parent.parent / 'resources' / 'solves' / 'AKQ-Game.gto'
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
---
+++ src/test_gto.py
@@ -0,0 +1,73 @@
+from pathlib  import Path
+import unittest
+from gto import GTO
+
+class TestGTO(unittest.TestCase):
+    akq_game = Path(__file__).parent.parent / 'resources' / 'solves' / 'AKQ-Game.gto'
+    solver = GTO()
+
+    def test_connect_and_disconnect(self):
+        s = self.solver
+        self.assertEqual(s.connect(), "You are connected to GTO+")
+        s.disconnect()
+        self.assertIsNone(s.sock)
+
+    def test_load_file(self):
+        s = self.solver
+        s.connect()
+        self.assertEqual(s.load_file(self.akq_game), "File successfully loaded.")
+        s.disconnect()
+
+    def test_request_node_data(self):
+        s = self.solver
+        s.connect()
+        s.load_file(self.akq_game)
+        node_data = s.request_node_data()
+        oop = node_data['oop']
+        ip = node_data['ip']
+        print(oop)
+        self.assertEqual(['Bet 1', 'Check'], node_data['actions'])
+        self.assertEqual('oop', node_data['pos'])
+        # TODO: test oop, ip
+        s.disconnect()
+
+    def test_request_pot_stacks(self):
+        s = self.solver
+        s.connect()
+        s.load_file(self.akq_game)
+        ps = s.request_pot_stacks()
+        self.assertEqual(1.0, ps['pot'])
+        self.assertEqual(1.0, ps['oop_stack'])
+        self.assertEqual(1.0, ps['ip_stack'])
+        s.disconnect()
+
+    def test_request_current_line(self):
+        s = self.solver
+        s.connect()
+        s.load_file(self.akq_game)
+        self.assertEqual([], s.request_current_line())
+        s.disconnect()
+
+    def test_take_action(self):
+        s = self.solver
+        s.connect()
+        s.load_file(self.akq_game)
+        s.take_action(0)
+        nd = s.request_node_data()
+        self.assertEqual('ip', nd['pos'])
+        self.assertEqual(['Bet 1'], s.request_current_line())
+        
+        s.load_file(self.akq_game)
+        s.take_action(1)
+        nd = s.request_node_data()
+        self.assertEqual('ip', nd['pos'])
+        self.assertEqual(['Check'], s.request_current_line())
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
